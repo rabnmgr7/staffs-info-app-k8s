ff9fd3cc68c6f5b4b287f8192c220ffe2150c9f2
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-09 21:30:57 +0545

    Update img-build.sh

--- img-build.sh
+++ img-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker image rm -f rabnmgr7/staffs-info-app/node:latest rabnmgr7/staffs-info-app/db:latest || true
-docker build -t rabnmgr7/staffs-info-app/node:latest ./node/
-docker build -t rabnmgr7/staffs-info-app/db:latest ./db/
+docker image rm -f rabnmgr7/staffs-info-app-node:latest rabnmgr7/staffs-info-app-db:latest || true
+docker build -t rabnmgr7/staffs-info-app-node:latest ./node/
+docker build -t rabnmgr7/staffs-info-app-db:latest ./db/
