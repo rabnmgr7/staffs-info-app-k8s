ad379e7de2e5d3c9472a18f3a30c6f1cdfa73d19
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-09 20:38:40 +0545

    Update run-container.sh

--- run-container.sh
+++ run-container.sh
@@ -6,5 +6,5 @@ docker network rm staffs-info-net || true
 echo "Creating network"
 docker network create --subnet 10.0.10.0/24 --gateway 10.0.10.1 --driver=bridge staffs-info-net || true
 echo "Creating containers"
-docker container run -itd --name staffs-info-db-cont --network staffs-info-net --ip 10.0.10.2 --restart always -p 3318:3306 harbor.registry.local/staffs-info-app/db:latest
-docker container run -itd --name staffs-info-node-cont --network staffs-info-net --ip 10.0.10.3 --restart always -v ./node/api:/app/api -p 8081:8123 harbor.registry.local/staffs-info-app/node:latest
+docker container run -itd --name staffs-info-db-cont --network staffs-info-net --ip 10.0.10.2 --restart always -p 3318:3306 rabnmgr7/staffs-info-app/db:latest
+docker container run -itd --name staffs-info-node-cont --network staffs-info-net --ip 10.0.10.3 --restart always -v ./node/api:/app/api -p 8081:8123 rabnmgr7/staffs-info-app/node:latest
