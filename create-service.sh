6cd759e843972d9b9916920bc14fec9daa7152c9
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-10 00:05:45 +0545

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -5,7 +5,7 @@ kubectl delete secret generic staffs-info-app-db-secret | true
 kubectl create configmap staffs-info-app-db-config --from-env-file=.env
 kubectl create secret generic staffs-info-app-db-secret --from-env-file=.env
 #creating services
-kubectl apply -f ./k8s-specifications/db-deployment.yaml
-kubectl apply -f ./k8s-specifications/db-service.yaml
-kubectl apply -f ./k8s-specifications/node-deployment.yaml
-kubectl apply -f ./k8s-specifications/node-service.yaml
+kubectl apply -f ../k8s-specifications/db-deployment.yaml
+kubectl apply -f ../k8s-specifications/db-service.yaml
+kubectl apply -f ../k8s-specifications/node-deployment.yaml
+kubectl apply -f ../k8s-specifications/node-service.yaml
