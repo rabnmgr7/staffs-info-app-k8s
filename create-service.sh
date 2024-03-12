5372a4650864c323f15fb5393d96db213db9a6fb
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-12 21:13:09 +0545

    service delete steps added

--- create-service.sh
+++ create-service.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-#Create a ConfigMap
+#Deleting Existing deployments and services
+kubectl delete deployment staffs-info-app-db-deployment | true
+kubectl delete deployment staffs-info-app-node-deployment | true
+kubectl delete service staffs-info-app-db-svc | true
+kubectl delete service staffs-info-app-node-svc | true
+
+#Deleting ConfigMaps and secrets
 kubectl delete configmap staffs-info-app-db-config | true
 kubectl delete secret generic staffs-info-app-db-secret | true
+
+#Creating New ConfigMaps and secrets
 kubectl create configmap staffs-info-app-db-config --from-env-file=.env
 kubectl create secret generic staffs-info-app-db-secret --from-env-file=.env
-#creating services
+
+#Creating services
 kubectl apply -f ./k8s-specifications/db-deployment.yaml
 kubectl apply -f ./k8s-specifications/db-service.yaml
 kubectl apply -f ./k8s-specifications/node-deployment.yaml
