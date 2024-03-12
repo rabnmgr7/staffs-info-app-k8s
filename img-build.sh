b8285627027331135dbbf3ec68d8ac01c8d1e1b2
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-12 21:20:42 +0545

    $BUILD_NUMBER tagging option added

--- img-build.sh
+++ img-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker image rm -f rabnmgr7/staffs-info-app-node:latest rabnmgr7/staffs-info-app-db:latest || true
-docker build -t rabnmgr7/staffs-info-app-node:latest ./node/
-docker build -t rabnmgr7/staffs-info-app-db:latest ./db/
+docker build -t rabnmgr7/staffs-info-app-node:$BUILD_NUMBER -t rabnmgr7/staffs-info-app-node:latest ./node/
+docker build -t rabnmgr7/staffs-info-app-db:$BUILD_NUMBER -t rabnmgr7/staffs-info-app-db:latest ./db/
