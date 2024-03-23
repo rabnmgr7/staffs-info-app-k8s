dd231911e4a283b5a8950bd0140c1711c529b706
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-23 22:56:09 +0545

    port forwarding added and getting service infos

--- create-service.sh
+++ create-service.sh
@@ -18,4 +18,9 @@ kubectl apply -f ./k8s-specifications/db-deployment.yaml
 kubectl apply -f ./k8s-specifications/db-service.yaml
 kubectl apply -f ./k8s-specifications/node-deployment.yaml
 kubectl apply -f ./k8s-specifications/node-service.yaml
-kubectl port-forward service/staffs-info-app-node-svc 30010:30010 --address 0.0.0.0
+kubectl port-forward service/staffs-info-app-node-svc 30010:30010 --address 0.0.0.0 & | true
+
+#Getting Deployments and Services Information
+kubectl get deployments
+kubectl get pods
+kubectl get services
