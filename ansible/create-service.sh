11d9b9a129186eb1d34f00355410c932d9faa887
rabnmgr7 <rabnmgr7@gmail.com>
2024-03-26 19:38:36 +0545

    home dir removed by ~

--- ansible/create-service.sh
+++ ansible/create-service.sh
@@ -10,14 +10,14 @@ kubectl delete configmap staffs-info-app-db-config | true
 kubectl delete secret generic staffs-info-app-db-secret | true
 
 #Creating New ConfigMaps and secrets
-kubectl create configmap staffs-info-app-db-config --from-env-file=/home/shesh/k8s/k8s-specifications/.config
-kubectl create secret generic staffs-info-app-db-secret --from-env-file=/home/shesh/k8s/k8s-specifications/.env
+kubectl create configmap staffs-info-app-db-config --from-env-file=~/k8s/k8s-specifications/.config
+kubectl create secret generic staffs-info-app-db-secret --from-env-file=~/k8s/k8s-specifications/.env
 
 #Creating services
-kubectl apply -f /home/shesh/k8s/k8s-specifications/db-deployment.yaml
-kubectl apply -f /home/shesh/k8s/k8s-specifications/db-service.yaml
-kubectl apply -f /home/shesh/k8s/k8s-specifications/node-deployment.yaml
-kubectl apply -f /home/shesh/k8s/k8s-specifications/node-service.yaml
+kubectl apply -f ~/k8s/k8s-specifications/db-deployment.yaml
+kubectl apply -f ~/k8s/k8s-specifications/db-service.yaml
+kubectl apply -f ~/k8s/k8s-specifications/node-deployment.yaml
+kubectl apply -f ~/k8s/k8s-specifications/node-service.yaml
 kubectl port-forward service/staffs-info-app-node-svc 30010:30010 --address 0.0.0.0 &
 sudo lsof -i :30010
 
