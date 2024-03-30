c5d74ebe456a6fe6d913b1640c1f1b2989e9a07b
rabnmgr7 <rabnmgr7@gmail.com>
2024-03-30 11:12:56 +0545

    loop used in scripts

--- img-push.sh
+++ img-push.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-docker image push rabnmgr7/staffs-info-app-node:$BUILD_NUMBER
-docker image push rabnmgr7/staffs-info-app-node:latest
-docker image push rabnmgr7/staffs-info-app-db:$BUILD_NUMBER
-docker image push rabnmgr7/staffs-info-app-db:latest
+node="rabnmgr7/staffs-info-app-node"
+db="rabnmgr7/staffs-info-app-db"
+
+# List of image names to remove
+images=(
+    "$node:$BUILD_NUMBER"
+    "$node:latest"
+    "$db:$BUILD_NUMBER"
+    "$db:latest"
+)
+
+#To which host this script is running
+echo "Running on host: $HOSTNAME"
+#Loop over the array and remove each image
+for image in "${images[@]}"; do
+    docker image push "$image"
+done
