f444989778f9719bb574188e1a5c979c444feadf
rabnmgr7 <rabnmgr7@gmail.com>
2024-03-30 10:32:15 +0545

    img-remove.sh created

---
+++ img-remove.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+node="staffs-info-app-node"
+db="staffs-info-app-db"
+
+# List of image names to remove
+images=(
+    "$node:$BUILD_NUMBER"
+    "$node:latest"
+    "$db:$BUILD_NUMBER"
+    "$db:latest"
+)
+
+#Loop over the array and remove each image
+for image in "${images[@]}"; do
+    docker image rm -f "$image"
+done
